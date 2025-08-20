b896e254e58b7ab6fd52e5779a4a56a998f3bcb8
Christophe Collot <christophe.collot.cloud@gmail.com>
2025-08-20 10:50:57 +0200

    upload config

---
+++ nvim/lua/plugins/kcl.lua
@@ -0,0 +1,22 @@
+vim.filetype.add({
+  extension = {
+    k = "kcl",
+    kcl = "kcl",
+  },
+})
+return {
+  {
+    "kcl-lang/kcl.nvim",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        kcl = {
+          mason = false,
+        },
+      },
+    },
+    setup = {},
+  },
+}
